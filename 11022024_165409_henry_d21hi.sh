3d6f0a684e55f25beeebc156b6818b9259e816c3
salamaleiqum <bachukigegia349@gmail.com>
2024-02-11 16:54:09 +0000

    Setting henry

---
+++ 11022024_165409_henry_d21hi.sh
@@ -0,0 +1,1 @@
+The word "limelight" that is used in theatre to refer to the performers on the stage originated because before electricity was available lime was burned in a lamp, which created a white light that was directed at the performers
